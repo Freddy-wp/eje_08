314c9e5184644190968d7e88f7cc5517c3c65e22
agus6607 <agusgeajose@gmail.com>
2020-09-12 20:55:51 -0400

    freddy

---
+++ src/test/java/py/edu/facitec/freddy_escurra_eje8/FreddyEscurraEje8ApplicationTests.java
@@ -0,0 +1,13 @@
+package py.edu.facitec.freddy_escurra_eje8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreddyEscurraEje8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
